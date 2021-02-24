d62d05e652b6377607d24238e703ecbdf3654231
Mark McQuillen <markdmcq@hotmail.com>
2021-02-24 17:35:54 -0500

    add button to get joke

--- src/App.js
+++ src/App.js
@@ -9,29 +9,34 @@ class App extends Component {
   super(props);
   this.handleClick = this.handleClick.bind(this);
   }
-  
+
   state = {
     joke: []
   }
 
-  componentDidMount() {
-     fetch('https://official-joke-api.appspot.com/random_joke')
-     .then(res => res.json())
-     .then((data) => {
-       this.setState({ joke: data })
-       console.log("Mark has retrieved some data: ", data)
-     })
-     .catch(console.log)
-   }
+  // componentDidMount() {
+  //    fetch('https://official-joke-api.appspot.com/random_joke')
+  //    .then(res => res.json())
+  //    .then((data) => {
+  //      this.setState({ joke: data })
+  //      console.log("Mark has retrieved some data: ", data)
+  //    })
+  //    .catch(console.log)
+  //  }
 
    handleClick() {
-   console.log('Click happened');
+   fetch('https://official-joke-api.appspot.com/random_joke')
+   .then(res => res.json())
+   .then((data) => {
+     this.setState({ joke: data })
+   })
+   .catch(console.log)
  }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>Click Me</button>
+        <button className="Click-here" onClick={this.handleClick}>Click Me for a Joke</button>
         <Joke joke={this.state.joke} />
       </div>
     );
