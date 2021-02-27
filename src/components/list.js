9369e810066f727ac37ea2ee195053fcf80727a9
Mark McQuillen <markdmcq@hotmail.com>
2021-02-27 14:47:53 -0500

    console.log when punchline is clicked

--- src/components/list.js
+++ src/components/list.js
@@ -1,20 +1,52 @@
-// src/components/contacts.js
-
-import React from 'react'
-
-const List = ({ list }) => {
-  return (
-    <div>
-      {list.map((contact) => (
-        <button key={contact.punchline} className="Click-here">{contact.punchline}</button>
-
-      ))}
-    </div>
-  )
-};
-
-export default List
+// // src/components/contacts.js
+//
+// import React from 'react'
+//
+// const List = ({ list }) => {
+//   handleClick() {
+//     console.log("Someone clicked a punchline!!!")
+//   }
+//
+//   return (
+//     <div>
+//       {list.map((contact) => (
+//         <button key={contact.punchline} className="Click-here" onClick={this.handleClick}>{contact.punchline}</button>
+//
+//       ))}
+//     </div>
+//   )
+// };
+//
+// export default List
 
 
 
 // <h5 key={contact.punchline} className="card-title Click-here">{contact.punchline}</h5>
+import React from 'react';
+
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleAnswer = this.handleAnswer.bind(this);
+  }
+
+  handleAnswer(e) {
+    // const name = e.target.value;
+    // this.props.onChange(name);
+    console.log("Someone clicked a punchline")
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.list.map((contact) => (
+          <button key={contact.punchline} className="Click-here" onClick={this.handleAnswer}>{contact.punchline}</button>
+
+        ))}
+      </div>
+    )
+  }
+}
+
+export default List
